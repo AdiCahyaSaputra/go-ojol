1d0bf313615617e8a5fd5da3817d1cc454791187
Adi <adics631@gmail.com>
2026-08-20 08:59:19 +0700

    feat(trip/dispatch): update dispatch routes to use POST and add authentication middleware

--- backend/trip/modules/dispatch/routes.go
+++ backend/trip/modules/dispatch/routes.go
@@ -1,18 +1,21 @@
 package dispatch
 
 import (
+	"github.com/AdiCahyaSaputra/go-ojol/backend/trip/middlewares"
 	"github.com/AdiCahyaSaputra/go-ojol/backend/trip/modules/dispatch/controller"
 	"github.com/AdiCahyaSaputra/go-ojol/backend/trip/pkg/constants"
+	"github.com/AdiCahyaSaputra/go-ojol/backend/trip/pkg/jwks"
 	"github.com/gin-gonic/gin"
 	"github.com/samber/do"
 )
 
 func RegisterRoutes(server *gin.Engine, injector *do.Injector) {
 	dispatchController := do.MustInvoke[controller.DispatchController](injector)
+	verifier := do.MustInvokeNamed[jwks.Verifier](injector, constants.JWKSVerifier)
 
 	dispatchRoutes := server.Group(constants.ROUTE_GROUP + "/dispatch")
 	{
-		dispatchRoutes.GET("/calculate-argo", dispatchController.CalculateArgo)
-		dispatchRoutes.GET("/find-driver", dispatchController.FindDriver)
+		dispatchRoutes.POST("/calculate-argo", middlewares.Authenticate(verifier), dispatchController.CalculateArgo)
+		dispatchRoutes.POST("/find-driver", middlewares.Authenticate(verifier), dispatchController.FindDriver)
 	}
 }
